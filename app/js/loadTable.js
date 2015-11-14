4448ff422741a6ec59727085f75611b9fae73e09
Laurent Meyer <laurent@moid.de>
2015-11-14 22:01:47 +0100

    Finished the first version of it, adjusted the responsive design

--- app/js/loadTable.js
+++ app/js/loadTable.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
-  var template = $('#table-template').html();
-  var hand_template = Handlebars.compile(template);
-  var placeholder = $('#content-placeholder');
+  var scoreTemplate = $('#score-template').html();
+  var hand_score_template = Handlebars.compile(scoreTemplate);
+  var scorePlaceholder = $('#content-placeholder');
+  var bugsTemplate = $('#bugs-template').html();
+  var hand_bugs_template = Handlebars.compile(bugsTemplate);
+  var bugsPlaceHolder = $('#bugs-placeholder');
   Handlebars.registerHelper('score', score);
-  Handlebars.registerHelper('each_with_score_filter', sortWithScore)
+  Handlebars.registerHelper('each_with_score_filter', sortWithScore);
+  Handlebars.registerHelper('bier', bier);
+  Handlebars.registerHelper('defStatus', statusDefinition);
   $.get("../data/data.json", function(data, status, xhr){
-    var html = hand_template(data);
-    placeholder.append(html);
+    var html = hand_score_template(data);
+    scorePlaceholder.append(html);
+    var html2 = hand_bugs_template(data);
+    bugsPlaceHolder.append(html2);
   })
 });
 function sortWithScore(array, opts){
@@ -38,6 +45,31 @@ function score(tester) {
   }
   for (var review of reviews){
     score += 100;
+  }
+  return score;
+}
+
+function statusDefinition(integer){
+  if (integer == 0){
+    return "To be done";
+  }
+  if (integer == 1){
+    return "Solved";
+  }
+  if (integer == 2){
+    return "Will be done in next release";
+  }
+}
+
+function bier(tester){
+  var score = 0;
+  var bugs = tester.bugs;
+  var reviews = tester.reviews;
+  for (var bug of bugs){
+    score += 1;
+  }
+  for (var review of reviews){
+    score += 1;
   }
   return score;
 }
